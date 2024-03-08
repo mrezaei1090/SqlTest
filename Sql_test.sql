f9f0864bd9f4b489bc23f652ad0559a0f4440817
mrezaei1090 <m.rezaei1090@gmail.com>
2024-03-08 22:32:23 +0330

    commit initioalize

---
+++ Sql_test.sql
@@ -0,0 +1,19 @@
+
+-- create database dbtaha;
+
+use dbtaha;
+create table tbStudents(
+	 id int,
+    fname varchar(20),
+    lname varchar(20),
+    age int
+);
+
+use dbtaha;
+
+insert into tbstudents values 
+(2,'amirali','saki',12)
+
+Select * from tbstudents;
+select fname from
+tbstudents
